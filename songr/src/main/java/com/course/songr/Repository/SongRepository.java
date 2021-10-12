3d88b80a984a3b32ac2f692a07acb5130111af03
rawanazazi12 <rawanazazi12@gmail.com>
2021-10-12 23:40:10 +0300

    lab13

---
+++ songr/src/main/java/com/course/songr/Repository/SongRepository.java
@@ -0,0 +1,12 @@
+package com.course.songr.Repository;
+
+
+import com.course.songr.model.Song;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface SongRepository extends JpaRepository<Song, Long> {
+
+
+}
